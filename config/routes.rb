06267b821da2ab75f7fb625acf447507e0f1d044
codesliced <codesliced@gmail.com>
2014-05-12 14:39:49 -0700

    MVP welcome screen

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root :to => 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
