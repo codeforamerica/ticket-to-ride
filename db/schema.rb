c03b1fe5de16ec1d833119b1f3664e77451ba8f5
codesliced <codesliced@gmail.com>
2014-05-12 16:58:48 -0700

    DB schema changes with birthday

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512202635) do
+ActiveRecord::Schema.define(version: 20140512202636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20140512202635) do
     t.string  "first_name",                                 null: false
     t.string  "middle_name"
     t.string  "last_name",                                  null: false
-    t.date    "birth_date",                                 null: false
+    t.date    "birthday",                                   null: false
     t.string  "first_language"
     t.string  "second_language"
     t.date    "enrollment_date"
