bde6d2bf31af9aa7b2e46e77ac9c9ea1b7de214e
codesliced <codesliced@gmail.com>
2014-05-21 22:55:38 -0700

    WIP-Script to move background image horizontally across screen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,26 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+// // scrolling background
+
+//  // speed in milliseconds
+//  var scrollSpeed = 120;
+
+//  // set the default position
+//  var current = 0;
+
+//  // set the direction
+//  var direction = 'h';
+
+//  function bgscroll() {
+//      // 1 pixel row at a time
+//      current -= 1;
+
+//      // move the background with backgrond-position css properties
+//      $('body').css("backgroundPosition", (direction == 'h') ? current + "px 0" : "0 " + current + "px");
+//  }
+
+//  //Calls the scrolling function repeatedly
+//  setInterval("bgscroll()", scrollSpeed);
