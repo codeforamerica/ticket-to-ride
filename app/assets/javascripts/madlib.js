2e88542f0bdb20ab33f0499b7ae5c60010cbe5b6
codesliced <codesliced@gmail.com>
2014-05-24 22:14:40 -0700

    WIP sandbox with single popup close buttons at a time

--- app/assets/javascripts/madlib.js
+++ app/assets/javascripts/madlib.js
@@ -1,43 +1,43 @@
-// $(document).ready(function() {
-//     $(function () { $("[data-toggle='popover']").popover(); });
+$(document).ready(function() {
+    $(function () { $("[data-toggle='popover']").popover(); });
 
-// var ViewModel = function () {
-//     var self = this;
-//     self.guardians = ko.observableArray();
-//     self.guardians.push({
-//         id: 1,
-//         firstName: ko.observable('George'),
-//         lastName: ko.observable('Harrison'),
-//         email: ko.observable('george@beatles.com')
-//     })
+var ViewModel = function () {
+    var self = this;
+    self.guardians = ko.observableArray();
+    self.guardians.push({
+        id: 1,
+        firstName: ko.observable('George'),
+        lastName: ko.observable('Harrison'),
+        email: ko.observable('george@beatles.com')
+    })
 
-//     self.closePopover = function(guardian) {
-//         $('#popover' + guardian.id + '_click').popover('hide');
-//     };
-// }
+    self.closePopover = function(guardian) {
+        $('#popover' + guardian.id + '_click').popover('hide');
+    };
+}
 
-// ko.bindingHandlers.popUp = {
-//     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-//         var attribute = ko.utils.unwrapObservable(valueAccessor());
-//         var templateContent = attribute.content;
-//         var popOverTemplate = "<div class='popOverClass' id='" + attribute.id + "-popover'>" + $(templateContent).html() + "</div>";
-//         $(element).popover({
-//             placement: 'top',
-//             content: popOverTemplate,
-//             html: true,
-//             trigger: 'manual'
-//         });
-//         $(element).attr('id', "popover" + attribute.id + "_click");
-//         $(element).click(function (){
-//             $(".popoverClass").popover("hide");
-//             $(this).popover('toggle');
-//             var thePopover = document.getElementById(attribute.id + "-popover");
-//             childBindingContext = bindingContext.createChildContext(viewModel);
-//             ko.applyBindingsToDescendants(childBindingContext, thePopover)
-//         })
-//     }
-// }
+ko.bindingHandlers.popUp = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        var attribute = ko.utils.unwrapObservable(valueAccessor());
+        var templateContent = attribute.content;
+        var popOverTemplate = "<div class='popOverClass' id='" + attribute.id + "-popover'>" + $(templateContent).html() + "</div>";
+        $(element).popover({
+            placement: 'top',
+            content: popOverTemplate,
+            html: true,
+            trigger: 'manual'
+        });
+        $(element).attr('id', "popover" + attribute.id + "_click");
+        $(element).click(function (){
+            $(".popoverClass").popover("hide");
+            $(this).popover('toggle');
+            var thePopover = document.getElementById(attribute.id + "-popover");
+            childBindingContext = bindingContext.createChildContext(viewModel);
+            ko.applyBindingsToDescendants(childBindingContext, thePopover)
+        })
+    }
+}
 
-//   ko.applyBindings(new ViewModel());
+  ko.applyBindings(new ViewModel());
 
-// });
+});
---
+++ app/assets/javascripts/popsthatclose.js
@@ -0,0 +1,33 @@
+$.fn.extend({
+  popoverClosable: function (options) {
+    var defaults = {
+    template:
+              '<div class="popover">\
+              <div class="arrow"></div>\
+              <div class="popover-header">\
+              <button type="button" class="close" data-dismiss="popover" aria-hidden="true">&times;</button>\
+              <h3 class="popover-title"></h3>\
+              </div>\
+              <div class="popover-content"></div>\
+              </div>'
+    };
+    options = $.extend({}, defaults, options);
+    
+    var $popover_togglers = this;
+    $popover_togglers.popover(options);
+    
+    $popover_togglers.on('click', function (e) {
+    e.preventDefault();
+
+    $popover_togglers.not(this).popover('hide');
+  });
+
+    $('html').on('click', '[data-dismiss="popover"]', function (e) {
+      $popover_togglers.popover('hide');
+    });
+  }
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popoverClosable();
+});
