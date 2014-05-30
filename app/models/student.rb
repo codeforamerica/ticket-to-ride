0d6d54c1b6cb8d2541e6f65d71371dc4413ff97b
Jeff Maher <jeff@codeforamerica.org>
2014-05-29 22:23:43 -0700

    Reformatted indents in Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ActiveRecord::Base
-    has_many :guardians
-    belongs_to :school
-	  acts_as_birthday :birthday
-    enum gender: [:male, :female]
+  has_many :guardians
+  belongs_to :school
+  acts_as_birthday :birthday
+  enum gender: [:male, :female]
 end
