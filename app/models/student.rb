14b457dc1de331d450c8d474c9b0fdc47bf64bcf
Jeff Maher <jeff@codeforamerica.org>
2014-05-30 14:08:33 -0700

    Added StudentRace model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
   has_many :guardians
+  has_many :student_races
   belongs_to :school
   acts_as_birthday :birthday
   enum gender: [:male, :female]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530203310) do
+ActiveRecord::Schema.define(version: 20140530203808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,6 +93,14 @@ ActiveRecord::Schema.define(version: 20140530203310) do
     t.datetime "updated_at"
     t.boolean  "active",                   default: false
     t.string   "mailing_city"
+  end
+
+  create_table "student_races", force: true do |t|
+    t.integer  "student_id"
+    t.string   "race"
+    t.boolean  "active"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "students", force: true do |t|
