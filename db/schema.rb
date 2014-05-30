373337ba2da180a4eb5ac1a7ed8f9521aa6b4ee8
Jeff Maher <jeff@codeforamerica.org>
2014-05-30 14:04:59 -0700

    Added Hispanic flag for Student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530202148) do
+ActiveRecord::Schema.define(version: 20140530203310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,6 +129,7 @@ ActiveRecord::Schema.define(version: 20140530202148) do
     t.string   "home_city"
     t.string   "mailing_city"
     t.string   "birth_country"
+    t.boolean  "is_hispanic"
   end
 
   create_table "welcome_messages", force: true do |t|
