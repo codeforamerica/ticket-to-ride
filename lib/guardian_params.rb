9baecdc37b57cb309c046fc9c86cbaa89f087f4a
Jeff Maher <jeff@codeforamerica.org>
2014-05-30 13:15:45 -0700

    Updated permissions for Guardian and Student field updating

--- lib/guardian_params.rb
+++ lib/guardian_params.rb
@@ -2,7 +2,26 @@ module GuardianParams
   def guardian_params
     params.require(:guardian).permit(
         :first_name,
-        :last_name
+        :middle_name,
+        :last_name,
+        :mailing_street_address_1,
+        :mailing_street_address_2,
+        :mailing_zip_code,
+        :cell_phone,
+        :alt_phone,
+        :alt_phone_type,
+        :email,
+        :receive_emails,
+        :receive_sms,
+        :receive_postal_mail,
+        :receive_grade_notices,
+        :receive_conduct_notices,
+        :restricted,
+        :armed_service_branch,
+        :armed_service_rank,
+        :armed_service_duty_station,
+        :student_id,
+        :mailing_city
     )
   end
 end
