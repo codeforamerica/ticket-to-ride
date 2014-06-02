2391160876c9be3ded5439cfbd4bdf577a1761fc
Jeff Maher <jeff@codeforamerica.org>
2014-06-01 23:17:39 -0400

    Added city and state fields for contact people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601225103) do
+ActiveRecord::Schema.define(version: 20140602031545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20140601225103) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "active",                   default: false
+    t.string   "mailing_city"
+    t.string   "mailing_state"
   end
 
   create_table "districts", force: true do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 20140601225103) do
     t.datetime "updated_at"
     t.boolean  "active",                     default: false
     t.string   "mailing_city"
+    t.string   "relationship"
   end
 
   create_table "schools", force: true do |t|
