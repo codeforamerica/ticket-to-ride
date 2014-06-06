1cd62448b887c0c02f7ed1c3635b7e23703eacb3
codesliced <codesliced@gmail.com>
2014-06-05 22:42:38 -0400

    Added welcome page for Warwick

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root :to => 'welcome#index-west-warwick'
+  root :to => 'welcome#index-warwick'
 
   resources :enrollment
 
