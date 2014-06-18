6d3dae9992379b9040d6f7b16d5ab3ed0209e276
Jeff Maher <jeff@codeforamerica.org>
2014-06-18 16:14:43 -0700

    Added mailing state to Student

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618231031) do
+ActiveRecord::Schema.define(version: 20140618231317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,7 @@ ActiveRecord::Schema.define(version: 20140618231031) do
     t.integer  "estimated_graduation_year"
     t.integer  "grade"
     t.string   "home_state"
+    t.string   "mailing_state"
   end
 
   create_table "welcome_messages", force: true do |t|
