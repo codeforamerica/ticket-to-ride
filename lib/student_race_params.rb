2f92c6cd60ac8472acc05fff9ee30b584a5b0046
codesliced <codesliced@gmail.com>
2014-07-02 20:24:15 -0700

    Fixes typo

--- lib/student_race_params.rb
+++ lib/student_race_params.rb
@@ -2,7 +2,7 @@ module StudentRaceParams
   def student_race_params
     params.require(:student_race).permit(
         :primary_race
-        :additional_race
+        :additional_races
     )
   end
 end
