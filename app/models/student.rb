3e071cbaeea27ca43f60acfded5f4d2138b21a32
Jeff Maher <jeff@codeforamerica.org>
2014-07-18 23:12:33 -0400

    Removed old grade stuff

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,8 +15,6 @@ class Student < ActiveRecord::Base
 
   # Enum Validations
   enum gender: [:male, :female]
-  enum grade: [:pre_kindergarten, :kindergarten, :grade_1, :grade_2, :grade_3, :grade_4, :grade_5, :grade_6, :grade_7, :grade_8, :grade_9, :grade_10, :grade_11, :grade_12]
-  enum last_completed_grade: [:pre_k, :k, :first_grade, :second_grade, :third_grade, :fourth_grade, :fifth_grade, :sixth_grade, :seventh_grade, :eighth_grade, :freshman, :sophomore, :junior]
 
   # Names can only be letters, spaces, and dashes
   validates :first_name, format: { with: ModelConstants::LETTERS_SPACES_AND_DASHES_REGEX,
