79eec7e10167c845baee009729a7d2b722b9dc5c
Jeff Maher <jeff@codeforamerica.org>
2014-07-18 23:55:51 -0400

    Added street regex to model constants

--- app/models/model_constants.rb
+++ app/models/model_constants.rb
@@ -3,5 +3,6 @@ module ModelConstants
   # Regular expressions
   LETTERS_SPACES_AND_DASHES_REGEX = /\A(\p{L}|\s|-)+\z/
   LETTERS_NUMBERS_SPACES_AND_DASHES_REGEX = /\A(\p{L}|\d|\s|-)+\z/
+  STREET_ADDRESS_REGEX = /\A[\p{L}\d\s\.-]+\z/
 
 end
