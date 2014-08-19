6d8dd53e752bd10d35e551df0439132d0927ba4a
Jeff Maher <jeff@codeforamerica.org>
2014-08-18 18:24:48 -0700

    Added jQuery plugins from admin prototype

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,5 @@
+// This is a manifest file that'll be compiled into admin.js, which will include all the files
+// listed below.
+//= require jquery
+//= require custominput.jquery
+//= require select.jquery
