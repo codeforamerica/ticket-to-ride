a9a4df0a7e8e03c48e65ccf0c90006048f8fcf2e
Jeff Maher <jeff@codeforamerica.org>
2014-08-19 11:47:05 -0700

    Added controller logic for setting up a new central admin (WIP - needs to be connected to front end)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -12,10 +12,71 @@ class AdminController < ApplicationController
     return admin_login
   end
 
-  def central_start
-    render 'central_start'
+  # -----------
+  # Central Admin Setup
+  # -----------
+
+  def central_setup_welcome
+    render 'central_setup_welcome'
   end
 
+  def central_setup_info
+
+    @errors = {}
+
+    # GET
+    if request.method_symbol == :get
+      render 'central_setup_info'
+    end
+
+    # POST
+
+    # Empty checks
+    name = request.POST['name']
+    if name == nil
+      @errors[:name] = 'No name was entered'
+    end
+
+    email = request.POST['email']
+    if email == nil
+      @errors[:email] = 'E-mail address was not entered'
+    end
+
+    password = request.POST['password']
+    if password == nil
+      @errors[:password] = 'Password was not entered'
+    end
+
+    confirm_password = request.POST['confirm_password']
+    if confirm_password == nil
+      @errors[:confirm_password] = 'The password confirmation was not entered'
+    end
+
+    if @errors.any?
+      render 'central_setup_info'
+    end
+
+    # Create the object and check validations
+    central_admin = AdminUser.create(name: name, email: email, user_role: :central_admin)
+
+    if !central_admin.valid?
+      @errors = central_admin.errors
+      render 'central_setup_info'
+    end
+
+    central_admin.active = true
+    central_admin.save
+
+    render action: 'central_setup_confirm'
+  end
+
+  def central_setup_confirm
+    render 'central_setup_confirm'
+  end
+
+  # -----------
+  # Authentication/Authorization
+  # -----------
   def admin_login
     render 'admin_login'
   end
--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,24 @@
+# encoding: utf-8
+
 class AdminUser < ActiveRecord::Base
+  include ModelConstants
+
+  # -- Relationships --
   belongs_to :district
 
+  # -- Enumerations --
   enum user_role: [:central_admin, :district_admin]
+
+  # -- Validations --
+
+  # Names can only be letters, spaces, and dashes
+  validates :name, format: { with: ModelConstants::LETTERS_SPACES_AND_DASHES_REGEX,
+                                   message: 'Name can only have letters, spaces, and dashes',
+                                   allow_nil: true
+  }
+
+  validates :email, format: { with: ModelConstants::EMAIL_REGEX,
+                              message: 'E-mail address was not valid',
+                              allow_nil: true
+  }
 end
