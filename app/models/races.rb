73b641a58cd861a2f172aa6af4d9e1f89fbe24d0
Jeff Maher <jeff@codeforamerica.org>
2014-08-22 14:20:25 -0700

    Added comments to explain the Races module.

--- app/models/races.rb
+++ app/models/races.rb
@@ -1,6 +1,13 @@
+
+# This module has the fixed values used for races students may belong to.
+# These values are not only what get stored in the database, but are
+# also used to construct translation keys in the locale files.
+# In other words, if you change something in here, make sure the
+# the changes are updated in the database accordingly and that
+# translation keys are updated.
 module Races
 
-  # Text description, but also used for translation
+  # Text description, but also used for translation and database storage
   BLACK = 'black'
   WHITE = 'white'
   ASIAN = 'asian'
