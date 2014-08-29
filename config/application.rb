360dfb2163e37b969d1b75fe388a8c9e652d9909
Jeff Maher <jeff@codeforamerica.org>
2014-08-29 08:42:36 -0700

    Fixed a bug that broke the application on non-JRuby installs

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,18 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-begin
-    security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
-    restricted_field = security_class.get_declared_field('isRestricted')
-    restricted_field.accessible = true
-    restricted_field.set nil, false
-rescue ClassNotFoundException => e
-  # Handle Mac Java, etc not having this configuration setting
-  $stderr.print "Java told me: #{e}n"    
+if RUBY_PLATFORM == 'java'
+  begin
+      require 'java'
+      java_import 'java.lang.ClassNotFoundException'
+      security_class = java.lang.Class.for_name('javax.crypto.JceSecurity')
+      restricted_field = security_class.get_declared_field('isRestricted')
+      restricted_field.accessible = true
+      restricted_field.set nil, false
+  rescue ClassNotFoundException => e
+    # Handle Mac Java, etc not having this configuration setting
+    $stderr.print "Java told me: #{e}n"
+  end
 end
 
 
